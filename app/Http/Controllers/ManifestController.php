24675f01b2c7bdd9fae420b07cac5b6535958898
Nur Wachid <wachid@outlook.com>
2020-05-06 01:10:26 +0700

    update files

---
+++ app/Http/Controllers/ManifestController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\Manifest\ManifestService;
+
+/**
+ * Class ManifestController
+ * @package App\Http\Controllers
+ */
+class ManifestController extends Controller
+{
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function manifestJson()
+    {
+        $output = (new ManifestService)->generate();
+        return response()->json($output);
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function offline()
+    {
+        return view('vendor.pwa.offline');
+    }
+}
