dbd67b0a0d677f8bd853e9511fb5d2544703f862
Nur Wachid <wachid@outlook.com>
2020-05-21 14:14:20 +0000

    update seeder social

--- resources/views/blog/partials/blog/author.blade.php
+++ resources/views/blog/partials/blog/author.blade.php
@@ -1,17 +1,19 @@
 <div class="blog-author">
     <div class="media media-none--xs">
         <img src="{{ $blog->user->avatar }}" alt="{{ $blog->user->name }}" class="media-img-auto">
-        <div class="media-body">
-            <h4 class="item-title">{{ $blog->user->profile->full_name }}</h4>
-            <div class="item-subtitle">Author</div>
-            <p>{{ $blog->user->profile->biography }}</p>
-            <ul class="item-social">
-                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-            </ul>
-        </div>
+        @if($blog->user->profile)
+            <div class="media-body">
+                <h4 class="item-title">{{ $blog->user->profile->full_name }}</h4>
+                <div class="item-subtitle">Author</div>
+                <p>{{ $blog->user->profile->biography }}</p>
+                <ul class="item-social">
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                </ul>
+            </div>
+        @endif
     </div>
 </div>
